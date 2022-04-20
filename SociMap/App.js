26d2c642cdaa627fa864bcce58a8c395dcb5c63b
Lucas Berg <lucas.t.berg@live.se>
2022-04-20 13:55:09 +0200

    stuff

--- SociMap/App.js
+++ SociMap/App.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, StatusBar, LogBox  } from 'react-native';
 import PersonsView from './Pages/PersonsView';
@@ -54,41 +53,6 @@ export default function App() {
         <PersonsView></PersonsView>
     </SafeAreaView>
     );
-=======
-import EditPersonScreen from './assets/screens/EditPersonScreen';
-
-// Added this for navigation to other page, refine to have button OnClick or just remove 
-import {NavigationContainer} from '@react-navigation/native';
-import PersonScreen from './assets/screens/PersonScreen';
-import {createStackNavigator} from '@react-navigation/stack';
-// END 
-
-const app = initializeApp({
-  apiKey: "AIzaSyARq36sGLC1ltpfqVMeMjgx-v5nbm7Ev5w",
-  authDomain: "socimap-2b18e.firebaseapp.com",
-  projectId: "socimap-2b18e",
-  storageBucket: "socimap-2b18e.appspot.com",
-  messagingSenderId: "1038195680118",
-  appId: "1:1038195680118:web:92051d9ccc3fee341d5057",
-  measurementId: "G-DB1JX1T6L8"
-})
-
-export default function App() {
-  // TODO: Used for navigation to other page at developing stage 
-  const Stack = createStackNavigator();
-  
-  return (
-    <LoginScreen/>
-
-    // TODO: Shows PersonScreen when starting the app, used when developing PersonScreen  
-    //<NavigationContainer>
-    //  <Stack.Navigator initialRouteName="EditPersonScreen">
-    //    <Stack.Screen name="LoginScreen" component={LoginScreen} />
-    //    <Stack.Screen name="EditPersonScreen" component={EditPersonScreen} />
-    //  </Stack.Navigator>
-    //</NavigationContainer>
-  );
->>>>>>> origin/personPage
 }
 
 const styles = StyleSheet.create({
